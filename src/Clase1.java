34043c1245f0addbc845de9decbb095234b06571
dam <dam@203P06>
2022-02-04 11:50:08 +0100

    1 primera actualizacion

---
+++ src/Clase1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola buenas");
+        System.out.println("Soy Miguel Eduardo Melo");
+       
+    
+    
+    
+    }
+
+}
